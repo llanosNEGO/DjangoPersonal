dbc9d082055c1549e4e1d7fbd7d051c1f1a9cb22
Abed Llovera <negollovera@gmail.com>
2024-06-03 00:26:06 -0500

    panel admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Proyectos , Tareas
 
 # Register your models here.
+
+admin.site.register(Proyectos)
+admin.site.register(Tareas)
--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,13 @@ from django.db import models
 class Proyectos(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Tareas (models.Model):
     titulo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=100)
     proyecto = models.ForeignKey(Proyectos, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
